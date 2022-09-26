3e8b5ad0841b510ecf292e121025bcd7d71587ed
Russell Smith <resmithjr88@gmail.com>
2022-09-26 07:00:57 -0600

    sum pairs complete and passing

--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -3,6 +3,24 @@
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+function sumPairs(A, N) {
+    let numbers = new Map()
+    let solution = new Map()
+
+    A.forEach((e) => {
+        numbers.set(e)
+    })
+    
+    A.forEach((e) => {
+        let diff = N - e
+        if (diff !== e) {
+            if (numbers.has(diff)) {
+                solution.set(Math.min(e, diff), Math.max(e, diff))
+            }
+        }
+    })
+
+    return Array.from(solution)
+}
 
 module.exports = sumPairs;
