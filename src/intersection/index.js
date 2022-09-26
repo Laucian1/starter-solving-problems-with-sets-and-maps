550e14a06f150a80032f884f3debb6a7530ebb37
Russell Smith <resmithjr88@gmail.com>
2022-09-26 06:43:01 -0600

    Intersection algorithm passing

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,17 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+    let iMap = new Map()
+    let resultSet = new Set()
+    a.forEach((e) => {
+        iMap.set(e)
+    })
+
+    b.forEach((e) => {
+        if (iMap.has(e)) {
+            resultSet.add(e)
+        }
+    })
+    const result = [...resultSet]
+    return result
+}
 
 module.exports = intersection;
